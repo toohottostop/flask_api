1f05847541468ba3f6ab63c725cb85f5310a60ae
toohottostop <polezaevkolka@gmail.com>
2021-05-19 23:13:53 +0500

    remove "api.add_resource(...)" to src/routes.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 from src import app
-from src import api
-from src.routes import BikesListApi
-
-api.add_resource(BikesListApi, '/bikes', "/bikes/<string:uuid>", strict_slashes=False)
 
 if __name__ == '__main__':
     app.run()
