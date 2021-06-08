e275e7647c6cdd0d4f01a6306dd89f8c41dc4717
toohottostop <polezaevkolka@gmail.com>
2021-06-08 20:32:31 +0500

    make get method for getting all riders

--- src/resources/riders.py
+++ src/resources/riders.py
@@ -1,12 +1,18 @@
 from flask_restful import Resource
+
+from src.database.models import Rider
 from src.schemas.riders import RiderSchema
+from src import db
 
 
 class RidersListApi(Resource):
     rider_schema = RiderSchema()
 
     def get(self):
-        pass
+        riders = db.session.query(Rider).all()
+        if not riders:
+            return "", 404
+        return self.rider_schema.dump(riders, many=True), 200
 
     def post(self):
         pass
