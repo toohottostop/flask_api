5bc233b4ee3695a7927e7de9e0ab8981a165fd1a
toohottostop <polezaevkolka@gmail.com>
2021-07-06 18:45:28 +0500

    add secret key variable

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ BASE_DIR = pathlib.Path(__file__).parent
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(BASE_DIR / "data" / "flask_api_db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "you-will-newer-know"
