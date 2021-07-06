fa8ccc98aff8c462090345ef7fa98df61b1ccf01
toohottostop <polezaevkolka@gmail.com>
2021-07-06 18:45:05 +0500

    add routes for auth resources

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,9 @@
 from src import api
 from src.resources.bikes import BikesListApi
 from src.resources.riders import RidersListApi
+from src.resources.auth import AuthRegister, AuthLogin
 
 api.add_resource(BikesListApi, "/bikes", "/bikes/<string:uuid>", strict_slashes=False)
 api.add_resource(RidersListApi, "/riders", strict_slashes=False)
+api.add_resource(AuthRegister, "/register", strict_slashes=False)
+api.add_resource(AuthLogin, "/login", strict_slashes=False)
